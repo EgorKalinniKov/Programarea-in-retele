51d636a788f39aef01d2477f63d33c21df0e8fc1
EgorKalinniKov <calinnicov@gmail.com>
2025-04-28 13:15:09 +0300

    Лабораторная Работа 3

---
+++ Lab3/Views/Shared/Error.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.View.Shared
+{
+    public class ErrorModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ Lab3/Views/ToDo/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Lab3.Views.ToDo
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
